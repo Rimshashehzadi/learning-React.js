23781ad9f603a72f0fb033a0633c69de0861cb92
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-05-23 06:28:04 -0700

    add

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='max-w-2xl mx-auto p-4'>
+      <h1 className='text-3xl font-bold underline text-center text-red-500'>
+        Hello world!
+      </h1>
+      <p className='text-lg text-gray-700 leading-relaxed  mt-4'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum sequi error sit consequuntur nesciunt consectetur maiores, accusamus corrupti exercitationem incidunt, minus fugit facilis magni nemo consequatur culpa praesentium ut eveniet?
+      </p>
+      <h2 className='text-2xl font-bold text-center text-blue-500 mt-6'>
+        this is vite project
+      </h2>
+    </div>
   )
 }
 
