72dc6cce5c75e1e0d196895ae4caa29e33c7b9ad
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-05-31 09:24:10 -0700

    add

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -3,15 +3,17 @@ import Components from './Components'
 import {Components2,name} from './Components'
 import { Exercise01 } from './Exercise01'
 import Function from './Function'
+import States from './States'
 
 const App = () => {
   return (
     <div className='max-w-2xl mx-auto p-4'>
      <Components/>
      <Components2/>
-     <h2>{name}</h2>
+     {/* <h2>{name}</h2> */}
      <Exercise01/>
      <Function/>
+     <States/>
     </div>
   )
 }
