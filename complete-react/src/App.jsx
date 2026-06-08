14f77b9e01bada96a58a020db38a1948082d8a15
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-06-08 04:11:57 -0700

    add

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -7,7 +7,7 @@ import States from './States'
 
 const App = () => {
   return (
-    <div className='max-w-2xl mx-auto p-4'>
+    <div className='max-w-4xl mx-auto p-4'>
      <Components/>
      <Components2/>
      {/* <h2>{name}</h2> */}
--- complete-react/src/States.jsx
+++ complete-react/src/States.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const States = () => {
+  const[fruit, setFruit] = useState('banana')
+  const changeFruit = () => {
+    setFruit('apple')
+  // let fruit = 'banana'
+  // const changeFruit = () => {
+  //   fruit = 'apple'
+  //   console.log(fruit)
+  }
   return (
-    <div>States</div>
+    <div>
+      <h1>React States</h1>
+      <h1>{fruit}</h1>
+      <button className='bg-blue-500 text-white p-2 rounded' onClick={changeFruit}>Change Fruit</button>
+    </div>
   )
 }
 
