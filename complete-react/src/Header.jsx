76d37460e56c094e96d130c1923e1af3d721c645
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-06-29 10:13:43 -0700

    add

--- complete-react/src/Header.jsx
+++ complete-react/src/Header.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross2 } from "react-icons/rx";
 import { Link } from "react-router-dom";
+// import Button from "./Button";
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
+    const [count,setCount] = useState(0)
     return (
         <div>
             <div className=" flex items-center justify-between p-4 bg-gray-200 rounded-lg mb-4">
@@ -67,7 +69,18 @@ const Header = () => {
 
             </div>
             <Button text='login' />
+<div>
+    <h1>{count}</h1>
+    <button className="p-4 rounded-2xl text-amber-50 bg-amber-700" onClick={()=>{setCount(count+1)}}>increase</button>
+{
+    count ==0 ? <h1>counter 0</h1>
+    :count ==1 ? <h1> Counter 1</h1>
+    :count ==2 ? <h1>Counter 2</h1>
+    :count ==3 ? <h1>Counter 3</h1>
+    :<h1>Others</h1>
+}
 
+</div>
 
         </div>
     )
--- complete-react/src/pages/Hooks.jsx
+++ complete-react/src/pages/Hooks.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+import { RxHamburgerMenu } from "react-icons/rx";
 import Button from '../Button'
 const Hooks = () => {
     // let fruit = 'Apple'
     const [fruit, setFruit] = useState('Apple')
     const [count, setCount] = useState(0)
     const [isVisible, setIsVisible] = useState(true)
+    const [isOpen, setIsOpen] = useState(false)
     const updateFruit = () => {
         // fruit = 'MANGO'
         // console.log(fruit)
@@ -31,10 +34,49 @@ const Hooks = () => {
             {/* TOGGLE (HIDE AND SHOW) */}
             <div className="mt-4 space-y-4">
                 <h1>Toggle</h1>
-               
+
                 <button className="p-6 bg-amber-600 text-amber-50 rounded-2xl" onClick={() => setIsVisible(!isVisible)}>Toggle</button>
-                 {isVisible? <h1 className="text-4xl font-bold">This is Toggle</h1> : null}
+                {isVisible ? <h1 className="text-4xl font-bold">This is Toggle</h1> : null}
+            </div>
+            {/* header using toggle button */}
+            <div className="flex justify-between items-center p-4 bg-amber-500 text-amber-50 rounded-2xl mt-4">
+                <div>logo</div>
+                <ul className='space-x-4 hidden md:flex'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                <div className="hidden md:flex">
+                    {/* <button className="p-2 bg-amber-600 text-amber-50 rounded-2xl">log in</button> */}
+                <button className="p-2.5 bg-amber-50 text-amber-600  text-xl rounded-2xl">log in</button>
                 </div>
+                {/* for mobile */}
+                <div className="md:hidden">
+                    <button>
+                        {isOpen ? <RxCross2 className="text-4xl" onClick={() => setIsOpen(!isOpen)} />
+                            : <RxHamburgerMenu className="text-4xl" onClick={() => setIsOpen(!isOpen)} />}
+                    </button>
+
+
+
+                </div>
+               
+            </div>
+             {/* mobile menu */}
+                {isOpen && (
+                    <div className="md:hidden space-y-2 justify-between items-center p-4 bg-amber-500 mt-1.5 text-amber-50  rounded-2xl">
+                        <ul className=" space-y-2 font-medium text-xl ">
+                           <li>Home</li>
+                           <li>About</li>
+                           <li>Contact</li>
+                        </ul>
+                        
+                             <button className="p-2.5 bg-amber-50 text-amber-600  text-xl rounded-2xl">log in</button>
+                        
+
+                    </div>
+                )}
+
         </div>
     )
 }
